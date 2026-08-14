605f5d6f20f3f2102b31d4f36144a4ee9fc88d77
Issk339 <86652865+User278-bot@users.noreply.github.com>
2026-08-14 22:05:08 +0900

    26.1 への対応

--- ghost-mod/src/client/java/com/ghost/init/GhostModKeyBindings.java
+++ ghost-mod/src/client/java/com/ghost/init/GhostModKeyBindings.java
@@ -3,11 +3,15 @@ package com.ghost.init;
 import com.ghost.config.GhostConfigScreen;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+//?if >= 26.1{
+/*import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+*///?}else{
+ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+//?}
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 //? if >=1.21.11 {
-/*import net.minecraft.resources.Identifier;
+/*import net.minecraft.resources.ResourceLocation;
 *///?}
 import org.lwjgl.glfw.GLFW;
 
@@ -24,17 +28,35 @@ public class GhostModKeyBindings {
      */
     public static void register() {
         // キーバインドの登録
+        //?if >= 26.1{
+        /*openConfigKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+                "key.ghostmod.openConfig", // 翻訳キー
+                InputConstants.Type.KEYSYM, // キーボード入力
+                GLFW.GLFW_KEY_G, // デフォルト: Gキー
+                KeyMapping.Category.register(ResourceLocation.fromNamespaceAndPath("ghostmod","keys"))
+                // カテゴリ翻訳キー
+        ));
+        *///?}else if >=1.21.11 {
+        
+        /*openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.ghostmod.openConfig", // 翻訳キー
+                InputConstants.Type.KEYSYM, // キーボード入力
+                GLFW.GLFW_KEY_G, // デフォルト: Gキー
+                KeyMapping.Category.register(ResourceLocation.fromNamespaceAndPath("ghostmod","keys"))
+                // カテゴリ翻訳キー
+        ));
+         
+        *///?}else{
+        
         openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.ghostmod.openConfig", // 翻訳キー
                 InputConstants.Type.KEYSYM, // キーボード入力
                 GLFW.GLFW_KEY_G, // デフォルト: Gキー
-                //? if >=1.21.11 {
-                /*KeyMapping.Category.register(Identifier.fromNamespaceAndPath("ghostmod","keys"))
-                *///?} else {
-                "category.ghostmod.keys" 
-                //?}
+               "category.ghostmod.keys"
                 // カテゴリ翻訳キー
         ));
+         
+        //?}
 
         // キー押下時のイベントハンドラを登録
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
